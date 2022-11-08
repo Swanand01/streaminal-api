1a5e531a169ab5cb2d95ac740cbfa39d2b7f7387
Swanand Mathekar <swanandmathekar@gmail.com>
2022-11-08 19:40:58 +0530

    Caching added for homepage responses

--- server.js
+++ server.js
@@ -8,24 +8,70 @@ app.use(cors())
 const server = require("http").Server(app);
 const port = process.env.PORT || 3000;
 
+let cache = {}
+
+const REQ_RES = {
+    "get-trending-movies": "trendingMovies",
+    "get-trending-tv-shows": "trendingTvShows",
+    "get-latest-movies": "latestMovies",
+    "get-latest-tv-shows": "latestTvShows"
+}
+
+function caching(req, res, next) {
+    let url = req.url;
+    url = url.slice(1);
+
+    if (cache[url]) {
+        let diff = Date.now() - cache[url].timestamp;
+        if (diff / 3600000 < 1) {
+            res.json({ [REQ_RES[url]]: cache[url].data });
+            return
+        }
+    }
+
+    next();
+}
+
+app.use(caching);
+
 app.get("/get-trending-movies", async (req, res) => {
     const trendingMovies = await getTrendingMedia({ movies: true });
     res.json({ "trendingMovies": trendingMovies });
+
+    cache["get-trending-movies"] = {
+        timestamp: Date.now(),
+        data: trendingMovies
+    };
 })
 
 app.get("/get-trending-tv-shows", async (req, res) => {
     const trendingTvShows = await getTrendingMedia({ tvShows: true });
     res.json({ "trendingTvShows": trendingTvShows });
+
+    cache["get-trending-tv-shows"] = {
+        timestamp: Date.now(),
+        data: trendingTvShows
+    };
 })
 
 app.get("/get-latest-movies", async (req, res) => {
     const latestMovies = await getLatestMedia({ movies: true });
     res.json({ "latestMovies": latestMovies });
+
+    cache["get-latest-movies"] = {
+        timestamp: Date.now(),
+        data: latestMovies
+    };
 })
 
 app.get("/get-latest-tv-shows", async (req, res) => {
     const latestTvShows = await getLatestMedia({ tvShows: true });
     res.json({ "latestTvShows": latestTvShows });
+
+    cache["get-latest-tv-shows"] = {
+        timestamp: Date.now(),
+        data: latestTvShows
+    };
 })
 
 server.listen(port);
